0db135412aca401c92479444347a5a5743881f66
Andradit <drakushev@mail.ru>
2024-05-12 16:18:41 +0300

    add changes 12.05.2024

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,13 +4,20 @@ from gendiff.parser import gendiff
 files = [('tests/fixtures/file3.json', 'tests/fixtures/file4.json'),
          ('tests/fixtures/file3.yaml', 'tests/fixtures/file4.yaml')]
 
+formatters = ['stylish', 'plain', 'json']
+
 with (open('tests/fixtures/nested_stylish.txt', 'r') as nested_stylish,
-      open('tests/fixtures/nested_plain.txt', 'r') as nested_plain):
-    expected_results = [(nested_stylish.read(), nested_plain.read())]
+      open('tests/fixtures/nested_plain.txt', 'r') as nested_plain,
+      open('tests/fixtures/nested_json.txt', 'r') as nested_json):
+    expected_results = [
+        nested_stylish.read(), nested_plain.read(), nested_json.read()]
 
 
 @pytest.mark.parametrize('file_1, file_2', files)
-@pytest.mark.parametrize('expected_1, expected_2', expected_results)
-def test_gendiff(file_1, file_2, expected_1, expected_2):
-    assert gendiff(file_1, file_2) == expected_1
-    assert gendiff(file_1, file_2, 'plain') == expected_2
+@pytest.mark.parametrize("form, expected", [
+    (formatters[0], expected_results[0]),
+    (formatters[1], expected_results[1]),
+    (formatters[2], expected_results[2])
+])
+def test_gendiff(file_1, file_2, form, expected):
+    assert gendiff(file_1, file_2, form) == expected
